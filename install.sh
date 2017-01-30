e5c13f0f50d022756478fd16ff866f7a31f6cb7f
Sergii Velykodnyi <iamvelykodnyi@gmail.com>
2017-01-30 15:12:33 +0200

    Rename bash files.
    Add prefix "bash" to the all bash files

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # ln -s ${BASEDIR}/vim/ ~/.vim
 
 # bash
-ln -s ${BASEDIR}/bash/.aliases ~/.aliases
+ln -s ${BASEDIR}/bash/.bash_aliases ~/.bash_aliases
 ln -s ${BASEDIR}/bash/.bash_profile ~/.bash_profile
 ln -s ${BASEDIR}/bash/.bash_prompt ~/.bash_prompt
 ln -s ${BASEDIR}/bash/.bashrc ~/.bashrc
-ln -s ${BASEDIR}/bash/.exports ~/.exports
-ln -s ${BASEDIR}/bash/.functions ~/.functions
+ln -s ${BASEDIR}/bash/.bash_exports ~/.bash_exports
+ln -s ${BASEDIR}/bash/.bash_functions ~/.bash_functions
 
 # git
 ln -s ${BASEDIR}/git/.gitconfig ~/.gitconfig
