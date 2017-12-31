c498f185f2f4c95709c7841131e8f49c95e109bb
Sergii Velykodnyi <sergii.velykodnyi@gmail.com>
2017-12-31 19:48:28 +0200

    Add gituser file

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ ln -s ${BASEDIR}/bash/.bash_exports ~/.bash_exports
 ln -s ${BASEDIR}/bash/.bash_functions ~/.bash_functions
 
 # git
+cp ${BASEDIR}/git/.gituser ~/.gituser
 ln -s ${BASEDIR}/git/.gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/git/.gitexcludes ~/.gitexcludes
 ln -s ${BASEDIR}/git/.git-completion.bash ~/.git-completion.bash
