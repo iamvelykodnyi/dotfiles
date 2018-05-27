17385757ec61838fa939e242061dbe2e2c603a2b
enter your name <enter your email>
2018-05-27 22:26:42 +0300

    Add zshrc

--- install.sh
+++ install.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SH_VARIANT=$1
 
+echo "Instal sh configuration"
+if [ "$SH_VARIANT" == "bash" ]; then
+  # bash
+  ln -s ${BASE_DIR}/bash/.bash_aliases ~/.bash_aliases
+  ln -s ${BASE_DIR}/bash/.bash_profile ~/.bash_profile
+  ln -s ${BASE_DIR}/bash/.bash_prompt ~/.bash_prompt
+  ln -s ${BASE_DIR}/bash/.bashrc ~/.bashrc
+  ln -s ${BASE_DIR}/bash/.bash_exports ~/.bash_exports
+  ln -s ${BASE_DIR}/bash/.bash_functions ~/.bash_functions
+else
+  # zsh
+  ln -s ${BASEDIR}/zsh/.zshrc ~/.zshrc
+fi
+
+# TODO: Add vim configuration
 # vim
-# ln -s ${BASEDIR}/vimrc ~/.vimrc
-# ln -s ${BASEDIR}/vim/ ~/.vim
+# echo "Install vim"
+# ln -s ${BASE_DIR}/vimrc ~/.vimrc
+# ln -s ${BASE_DIR}/vim/ ~/.vim
 
 # nvm
+echo "Install NVM"
 NVM_DIR="$HOME/.nvm"
 git clone https://github.com/creationix/nvm.git "$NVM_DIR"
 cd "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
-ln -s ${BASEDIR}/nvm/default-packages ~/.nvm/default-packages
-
-# bash
-ln -s ${BASEDIR}/bash/.bash_aliases ~/.bash_aliases
-ln -s ${BASEDIR}/bash/.bash_profile ~/.bash_profile
-ln -s ${BASEDIR}/bash/.bash_prompt ~/.bash_prompt
-ln -s ${BASEDIR}/bash/.bashrc ~/.bashrc
-ln -s ${BASEDIR}/bash/.bash_exports ~/.bash_exports
-ln -s ${BASEDIR}/bash/.bash_functions ~/.bash_functions
+ln -s ${BASE_DIR}/nvm/default-packages ~/.nvm/default-packages
 
 # git
-cp ${BASEDIR}/git/.gituser ~/.gituser
-ln -s ${BASEDIR}/git/.gitconfig ~/.gitconfig
-ln -s ${BASEDIR}/git/.gitexcludes ~/.gitexcludes
-ln -s ${BASEDIR}/git/.git-completion.bash ~/.git-completion.bash
+echo "Install git configuration"
+cp ${BASE_DIR}/git/.gituser ~/.gituser
+ln -s ${BASE_DIR}/git/.gitconfig ~/.gitconfig
+ln -s ${BASE_DIR}/git/.gitexcludes ~/.gitexcludes
+ln -s ${BASE_DIR}/git/.git-completion.bash ~/.git-completion.bash
--- uninstall.sh
+++ uninstall.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SH_VARIANT=$1
 
 # vim
-# ln -s ${BASEDIR}/vimrc ~/.vimrc
-# ln -s ${BASEDIR}/vim/ ~/.vim
+# TODO: Create UNINSTALL functionality
+# ln -s ${BASE_DIR}/vimrc ~/.vimrc
+# ln -s ${BASE_DIR}/vim/ ~/.vim
 
 # nvm
+# TODO: Create UNINSTALL functionality
 # NVM_DIR="$HOME/.nvm"
 # git clone https://github.com/creationix/nvm.git "$NVM_DIR"
 # cd "$NVM_DIR"
 # git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
-# ln -s ${BASEDIR}/nvm/default-packages ~/.nvm/default-packages
+# ln -s ${BASE_DIR}/nvm/default-packages ~/.nvm/default-packages
 
-# bash
-rm ~/.bash_aliases
-rm ~/.bash_profile
-rm ~/.bash_prompt
-rm ~/.bashrc
-rm ~/.bash_exports
-rm ~/.bash_functions
+if [ ${SH_VARIANT} == "bash" ]; then
+  # bash
+  echo "Uninstal bash configuration"
+
+else
+  # zsh
+  echo "Uninstal zsh configuration"
+fi
+
 
 # git
+echo "Uninstal git configuration"
 rm ~/.gituser
 rm ~/.gitconfig
 rm ~/.gitexcludes
