7f799068bb244ca632a64657889f9ecda6958032
Sergii Velykodnyi <sergii.velykodnyi@gmail.com>
2020-06-20 14:27:31 +0300

    Add Oh My Fish config

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Uninstall fish configuration..."
+for file in ~/.config/omf/{bundle,channel,theme,init.fish}; do
+  if [ -f ${file} ]; then
+    rm ${file}
+  fi
+done;
+unset file;
+
+# TODO: rewrite it to for-in loop lake above.
 echo "Uninstall git configuration..."
 rm ~/.gituser
 rm ~/.gitconfig
